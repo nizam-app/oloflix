eb57cc24d07531cc565d43666903963306658d2f
nizam-app <164641475+shahedroks@users.noreply.github.com>
2026-01-12 16:13:39 +0600

    full applicaiton done

--- lib/core/constants/api_control/global_api.dart
+++ lib/core/constants/api_control/global_api.dart
@@ -1,5 +1,5 @@
-//String api = "https://oloflix.tv/";
+String api = "https://oloflix.tv/";
 
-String api = "http://103.208.183.250:8000/";
+// String api = "http://103.208.183.250:8000/";
 
 //String api= "http://103.208.183.250:8000/" ;
